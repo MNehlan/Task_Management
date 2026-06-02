27d345223afcf41d92e98577af2820e0f2e15635
MNehlan <211360561+MNehlan@users.noreply.github.com>
2026-06-02 14:08:49 +0530

    feat: add dashboard page and statistics

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,64 @@
+import { useEffect, useState } from 'react';
+import api from '../api/api.js';
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [stats, setStats] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
-export default Dashboard
+  const fetchDashboard = async () => {
+    try {
+      setLoading(true)
+      const { data } = await api.get('/workspace/dashboard');
+      setStats(data.stats);
+    } catch (error) {
+      setError(
+        error.response?.data?.message ||
+        'No data found'
+      );
+    } finally {
+      setLoading(false)
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboard();
+  }, []);
+
+  if (loading) {
+    return <div>Loading..</div>
+  }
+
+  return (
+    <div className='grid grid-cols-2 md:grid-cols-3 gap-4'>
+      <div>
+        <p>Workspaces</p>
+        <p>{stats.workspaces}</p>
+      </div>
+
+      <div>
+        <p>Tasks</p>
+        <p>{stats.tasks}</p>
+      </div>
+      <div>
+        <p>Todo</p>
+        <p>{stats.todo}</p>
+      </div>
+      <div>
+        <p>Review</p>
+        <p>{stats.review}</p>
+      </div>
+      <div>
+        <p>In progress</p>
+        <p>{stats.inProgress}</p>
+      </div>
+      <div>
+        <p>Completed</p>
+        <p>{stats.completed}</p>
+      </div>
+
+    </div>
+  );
+};
+
+export default Dashboard;
