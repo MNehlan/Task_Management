2a925a8939f2426e8b03518dfd7fecf24efd50b7
MNehlan <211360561+MNehlan@users.noreply.github.com>
2026-07-19 12:34:28 +0530

    security: add Helmet middleware

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
 
-import { verifyToken, authorizeRoles } from './middlewares/authMiddleware.js';
+// Route imports
 import authRoutes from './routes/authRoutes.js';
 import workspaceRoutes from './routes/workspaceRoutes.js';
 import taskRoutes from './routes/taskRoutes.js';
@@ -14,13 +15,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors());
+// Middlewares
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
 
+// App Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/workspace', workspaceRoutes);
 app.use('/api/task', taskRoutes);
 
+// Admin Routes
 app.use('/api/admin', adminRoutes);
 
 mongoose
